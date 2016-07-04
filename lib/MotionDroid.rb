efdc242a4136cfe82da82f82dd1d53457d8478c7
Ajith Hussain <ajith.hussain@gmail.com>
2016-07-05 06:32:07 +1000

    Base gem

---
+++ lib/MotionDroid.rb
@@ -0,0 +1,9 @@
+require "MotionDroid/version"
+
+module MotionDroid
+  def linear_layout(&block)
+  end
+
+  def relative_layout(&block)
+  end
+end
---
+++ lib/MotionDroid/version.rb
@@ -0,0 +1,3 @@
+module MotionDroid
+  VERSION = "0.1.0"
+end
