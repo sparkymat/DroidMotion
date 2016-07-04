4f74f5602c4fde11a3a75770528ee6216c9a5d30
Ajith Hussain <csy0013@googlemail.com>
2016-07-05 08:08:32 +1000

    WIP: Layout classes / instance eval

--- lib/DroidMotion.rb
+++ lib/DroidMotion.rb
@@ -1,9 +1,17 @@
 require "DroidMotion/version"
 
 module DroidMotion
-  def linear_layout(&block)
+  class LinearLayout
   end
 
-  def relative_layout(&block)
+  class RelativeLayout
+  end
+
+  def linear_layout(opts, &block)
+    layout = ::DroidMotion::LinearLayout.new
+    layout.instance_eval(block)
+  end
+
+  def relative_layout(opts, &block)
   end
 end
